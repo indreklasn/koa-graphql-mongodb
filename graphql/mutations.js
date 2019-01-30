9c01c1d524ff95dac4ff6c664f649bdcb1414620
Indrek Lasn <lasnindrek@gmail.com>
2019-01-30 15:32:19 +0100

    add edit mutation

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -24,8 +24,34 @@ const Mutation = new GraphQLObjectType({
 
         return newGadget.save();
       }
-    }
+    },
+    updateGadget: {
+      type: gadgetGraphQLType,
+      args: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString }, 
+        release_date: { type: GraphQLString },
+        by_company: { type: GraphQLString },
+        price: { type: GraphQLString }
+      },
+      resolve(parent, args) {
+        Gadget.findById(args.id)
+          .then(gadget => {
+            gadget.name = args.name
+            gadget.release_date = args.release_date,
+            gadget.by_company = args.by_company,
+            gadget.price = args.price
 
+            return gadget.save()
+
+          })
+          .then(updatedGadget => {
+            console.log(updatedGadget)
+            return updatedGadget
+          })
+          .catch(err => console.log(err))
+      }
+    }
   }
 })
 
